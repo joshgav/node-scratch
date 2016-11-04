c4eadf0f1277e6a0f4e391e77e323c8bd43e9280
Josh Gavant <josh.gavant@outlook.com>
2016-11-04 13:26:29 -0700

    Add Winston support

--- bootstrap.js
+++ bootstrap.js
@@ -1,15 +1,28 @@
-var ikey = 'ce00d434-39b5-4a5b-bf0f-4fc6ce8380a7';
-var main = './index.js';
-process.env.APPINSIGHTS_INSTRUMENTATIONKEY = ikey;
+var appinsights   = require('applicationinsights');
+var winston       = require('winston');
+var app           = require('./package.json');
 
-var appinsights = require('applicationinsights');
+// start AppInsights data collection
+var ikey = 'ce00d434-39b5-4a5b-bf0f-4fc6ce8380a7';
+process.env.APPINSIGHTS_INSTRUMENTATIONKEY = ikey;
 appinsights.setup().start();
 
+// capture console.log output as traces
 let original_log = console.log;
 console.log = function new_log(message) {
   appinsights.getClient().trackTrace(message);
   original_log(message);
 }
 
-require(main);
+// register AppInsights to capture Winston traces
+var appinsights_transport =
+  require('winston-azure-application-insights').AzureApplicationInsightsLogger;
+
+winston.add(appinsights_transport, {
+  client: appinsights.getClient()
+});
+winston.info("AppInsights transport added to Winston.");
+
+// run the user's app
+require(`./${app.main}`);
 
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+var winston = require('winston');
+
 var http = require("http");
 var count = 0;
 var port = process.env.PORT || 8080;
 
 function basic_listener(req, res) {
   count++;
-  console.log(`request ${count} received`);
+  console.log(`console: request ${count} received`);
+  winston.info(`winston: request ${count} received`);
   res.writeHead( 200, {"Content-Type": "text/plain"} );
-  res.write(`response #${count} received\n`);
+  res.write(`http: response #${count} received\n`);
   res.end();
 }
 
 var http_server = http.createServer(basic_listener);
 http_server.listen(port);
 
-console.log(`${__filename}: http server listening on port ${port}`);
+console.log(`console: http server listening on port ${port}`);
+winston.info(`winston: http server listening on port ${port}`);
 
