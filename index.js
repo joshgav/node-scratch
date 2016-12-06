167505d871d472dc7527ea176c7dba52e2dd6627
Josh Gavant <josh.gavant@outlook.com>
2016-12-06 13:06:06 -0800

    add support for graceful exit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var winston = require('winston');
 
-var http = require("http");
+var url = require('url');
+var http = require('http');
 var count = 0;
 var port = process.env.PORT || 8080;
 
@@ -11,9 +12,21 @@ function basic_listener(req, res) {
   res.writeHead( 200, {"Content-Type": "text/plain"} );
   res.write(`http: response #${count} received\n`);
   res.end();
+  if (url.parse(req.url).path.startsWith('/stop')) {
+    console.log(`Request to: ${req.url}`);
+    stop();
+  }
+}
+
+function stop() {
+  console.log('Gracefully stopping process.');
+  process.exit(0);
 }
 
 var http_server = http.createServer(basic_listener);
+http_server.on('connection', (socket) => {
+  socket.setTimeout(0); // no keep-alive
+});
 http_server.listen(port);
 
 console.log(`console: http server listening on port ${port}`);
